b33a511fa48c7d022bee620327b8fa79b9a6b239
monie-B <mbullock091@icloud.com>
2023-01-08 14:54:58 -0500

    adding media queries

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -7,10 +7,16 @@ const UserProfile = (props) => {
       <NavBar />
         <div className='user-header'>
           <h1>Space for the Username</h1>
-          <div className='pic-section'>This space for user Avatar</div>
+          <div className='pic-section'>
+            <img src='user.png' alt='user icon' className='userpic' />
+          </div>
           <div className='user-detail-section'>
-            <button>Follow</button>
-            <button className='msgbtn'>Message</button>
+            <div className='btns'>
+              <button>Follow</button>
+              <button className='msgbtn'>Message</button>
+            </div>
+            <h3>User's actual Name goes here</h3>
+              <p>User bio/description goes here. Hello everyone! Welcome to my travel page. Message me if you have any further questions!</p>
           </div>
         </div>
     </>
