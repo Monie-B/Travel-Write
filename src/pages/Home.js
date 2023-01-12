a69b95b75d79e9da8a62bb3b3f325b94267286bb
monie-B <mbullock091@icloud.com>
2023-01-12 11:53:53 -0500

    homepage is updated and fully responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Footer from '../components/Footer'
 import { Header } from '../components/Header'
 import '../css/home.css'
+import '../css/blobz.css'
 
 const Home = () => {
   return (
@@ -38,6 +39,11 @@ const Home = () => {
                 Travel Write is your go to app for saving, blogging, and rating your travel experiences!
             </h1>
         </div>
+        {/* <div class="tk-blob">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 747.2 726.7">
+                <path d="M539.8 137.6c98.3 69 183.5 124 203 198.4 19.3 74.4-27.1 168.2-93.8 245-66.8 76.8-153.8 136.6-254.2 144.9-100.6 8.2-214.7-35.1-292.7-122.5S-18.1 384.1 7.4 259.8C33 135.6 126.3 19 228.5 2.2c102.1-16.8 213.2 66.3 311.3 135.4z"></path>
+            </svg>
+        </div> */}
         <div className='container2'>
             <div className='img-div2'>
                 <img src='https://rb.gy/3eo6vo' alt='kids on vacation at a waterpark' className='home-resort3' />
